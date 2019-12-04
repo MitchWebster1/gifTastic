12a7e54ccb90e661a5af610d4f04ae3d42ecd9f0
Mitch Webster <mitch83083@gmail.com>
2019-12-03 19:22:24 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # gifTastic
+
+[**https://mitchwebster1.github.io/gifTastic/**](https://mitchwebster1.github.io/gifTastic/)
+
+## What does this project do?
+
+This project pulls gifs from the giphy API based on your search.
+
+## Why is this project useful?
+
+This project was useful for practicing JavaScript and learning about API's
+
+## How can users get started with this project?
+
+Just click the link at the top and search for a gif category!
+
+## Where can users get help with your project?
+
+Please contact me if you need any assistance.
+
+## Who maintains and contributes to the project?
+
+I am the sole contributer.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,70 +1,73 @@
 const startingBtns = [
-  "motorcycle stunts",
-  "excited",
-  "austin powers",
-  "chuck norris",
-  "fail",
-  "dance",
-  "treadmill",
-  "weightlifting fail",
-];
-const select = id => document.getElementById(id);
-const createEl = el => document.createElement(el);
-const random = () => Math.floor(Math.random() * 50);
+  'motorcycle stunts',
+  'excited',
+  'austin powers',
+  'chuck norris',
+  'fail',
+  'dance',
+  'treadmill',
+  'weightlifting fail'
+]
+const select = id => document.getElementById(id)
+const createEl = el => document.createElement(el)
+const random = () => Math.floor(Math.random() * 50)
 
 // Used to empty the gifs out with a button click
 const empty = () => {
-  const el = select("gif");
+  const el = select('gif')
   while (el.firstChild) {
-    el.removeChild(el.firstChild);
+    el.removeChild(el.firstChild)
   }
-};
+}
 
-select("emptyDiv").addEventListener("click", empty);
+select('emptyDiv').addEventListener('click', empty)
 
 // Creates new buttons based on input of textbox
 const attach = (div, text, attachToEl, clas) => {
-  const el = createEl(div);
-  el.innerHTML = text;
-  el.value = text;
-  el.className = clas;
-  attachToEl.append(el);
-};
+  const el = createEl(div)
+  el.innerHTML = text
+  el.value = text
+  el.className = clas
+  attachToEl.append(el)
+}
 
-const createBtn = (event) => {
-  event.preventDefault();
-  const btnName = select("btnText").value;
-  attach("button", btnName, select("btns"), "btn");
-  select("btnForm").reset();
-};
+const createBtn = event => {
+  event.preventDefault()
+  const btnName = select('btnText').value
+  attach('button', btnName, select('btns'), 'btn')
+  select('btnForm').reset()
+}
 
-select("addBtn").addEventListener("click", createBtn);
-select("addBtn").addEventListener("keydown", createBtn);
+select('addBtn').addEventListener('click', createBtn)
+select('addBtn').addEventListener('keydown', createBtn)
 
 // ****************************************************
 
 // Assigns all the data attributes to image on creation
 const dataAssign = (response, i) => {
-  const imgData = createEl("img");
-  imgData.setAttribute("src", response.data[i].images.fixed_height_still.url);
-  imgData.setAttribute("data-still", response.data[i].images.fixed_height_still.url);
-  imgData.setAttribute("data-animate", response.data[i].images.fixed_height.url);
-  imgData.setAttribute("data-state", "still");
-  return imgData;
-};
+  const imgData = createEl('img')
+  imgData.setAttribute('src', response.data[i].images.fixed_height_still.url)
+  imgData.setAttribute(
+    'data-still',
+    response.data[i].images.fixed_height_still.url
+  )
+  imgData.setAttribute('data-animate', response.data[i].images.fixed_height.url)
+  imgData.setAttribute('data-state', 'still')
+  return imgData
+}
 
 // *****************************************************
 
 // Builds the div that the gif goes into then appends it to the main gif div
 const gifAttach = (attachToEl, response, i, clas) => {
-  const el = createEl("div");
-  const rating = createEl("P");
-  const img = dataAssign(response, i);
-  rating.innerHTML = `Rated: ${response.data[i].rating}`;
-  el.append(rating, img);
-  el.className = clas;
-  attachToEl.prepend(el);
-};
+  const el = createEl('div')
+  const rating = createEl('P')
+  const img = dataAssign(response, i)
+  rating.innerHTML = `Rated: ${response.data[i].rating}`
+  el.append(rating, img)
+  el.className = clas
+  attachToEl.prepend(el)
+}
 
 // ***********************************************************
 
@@ -72,78 +75,63 @@ const gifAttach = (attachToEl, response, i, clas) => {
 const request = (search, offset) => {
   $.ajax({
     url: `https://api.giphy.com/v1/gifs/search?api_key=FW6E7JrSMowL9qZhFstFGdSaEOVggJm3&q=${search}&limit=10&offset=${offset}&rating=PG-13&lang=en`,
-    method: "GET",
+    method: 'GET'
   })
-    .then((response) => {
+    .then(response => {
       for (let i = 0; i < response.data.length; i += 1) {
-        gifAttach(select("gif"), response, i, "gifDiv");
+        gifAttach(select('gif'), response, i, 'gifDiv')
       }
     })
-    .catch(error => console.error(error));
-};
+    .catch(error => console.error(error))
+}
 
-// const axios = require("axios");
+const searchVal = event => {
+  const searchTerm = event.target.value
+  const offset = random()
+  request(searchTerm, offset)
+}
 
-// const request = (search) => {
-//   axios
-//     .get(
-//       `https://api.giphy.com/v1/gifs/search?api_key=FW6E7JrSMowL9qZhFstFGdSaEOVggJm3&q=${search}&limit=10&offset=0&rating=PG-13&lang=en`,
-//     )
-//     .then((response) => {
-//       for (let i = 0; i < response.data.length; i += 1) {
-//         gifAttach(select("gif"), response, i, "gifDiv");
-//       }
-//     })
-//     .catch(error => console.error(error));
-// };
-
-const searchVal = (event) => {
-  const searchTerm = event.target.value;
-  const offset = random();
-  request(searchTerm, offset);
-};
-
-select("btns").addEventListener("click", searchVal);
+select('btns').addEventListener('click', searchVal)
 
 // *************************************************
 
 // Handles the animated and still image swapping
-const stopStartGif = (event) => {
-  const getData = data => event.target.getAttribute(data);
-  const setData = (attr, data) => event.target.setAttribute(attr, data);
-  const animate = getData("data-animate");
-  const still = getData("data-still");
-  const state = getData("data-state");
-  if (state === "still") {
-    setData("src", animate);
-    setData("data-state", "animate");
+const stopStartGif = event => {
+  const getData = data => event.target.getAttribute(data)
+  const setData = (attr, data) => event.target.setAttribute(attr, data)
+  const animate = getData('data-animate')
+  const still = getData('data-still')
+  const state = getData('data-state')
+  if (state === 'still') {
+    setData('src', animate)
+    setData('data-state', 'animate')
   } else {
-    setData("src", still);
-    setData("data-state", "still");
+    setData('src', still)
+    setData('data-state', 'still')
   }
-};
+}
 
-select("gif").addEventListener("click", stopStartGif);
+select('gif').addEventListener('click', stopStartGif)
 
 // ***************************************************
 
 // Creates buttons from array on page load / loads 10 trending gifs
 const start = () => {
   for (let i = 0; i < startingBtns.length; i += 1) {
-    const text = startingBtns[i];
-    attach("button", text, select("btns"), "btn");
+    const text = startingBtns[i]
+    attach('button', text, select('btns'), 'btn')
   }
   $.ajax({
     url:
-      "https://api.giphy.com/v1/gifs/trending?api_key=FW6E7JrSMowL9qZhFstFGdSaEOVggJm3&limit=10&rating=PG-13",
-    method: "GET",
+      'https://api.giphy.com/v1/gifs/trending?api_key=FW6E7JrSMowL9qZhFstFGdSaEOVggJm3&limit=10&rating=PG-13',
+    method: 'GET'
   })
-    .then((response) => {
+    .then(response => {
       for (let i = 0; i < response.data.length; i += 1) {
-        gifAttach(select("gif"), response, i, "gifDiv");
+        gifAttach(select('gif'), response, i, 'gifDiv')
       }
     })
-    .catch(error => console.error(error));
-};
+    .catch(error => console.error(error))
+}
 
-start();
+start()
